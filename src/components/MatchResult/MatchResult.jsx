83f77d311bdc7111870f2de95d4406d022fd753c
Kbaltazar55555 <kenneth.baltazarv@gmail.com>
2024-02-14 10:28:12 -0500

    added matchresult matchresultservices

--- src/components/MatchResult/MatchResult.jsx
+++ src/components/MatchResult/MatchResult.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import * as gameServices from './services/gameServices';
-import * as matchResultServices from './services/matchResultServices';
-import * as profileServices from './services/profileServices';
 import { ReactComponent as CloseIcon } from './assets/close-icon.svg'; // Importing an "X" icon
 
+//Get an ID for the match it is set to display 
 const MatchResult = ({ fromGame }) => { // Add a prop to indicate if this is from a game session
   const [matchResult, setMatchResult] = useState({});
   const [userProfiles, setUserProfiles] = useState({ player1: {}, player2: {} });
