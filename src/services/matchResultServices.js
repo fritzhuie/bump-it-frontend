feb321d186703da4f2a7de52708efd47dde3c00d
Kbaltazar55555 <kenneth.baltazarv@gmail.com>
2024-02-14 07:29:32 -0500

    added matchresult matchresultservices

---
+++ src/services/matchResultServices.js
@@ -0,0 +1,44 @@
+const BASE_URL = `https://django-bump-it-8e545e507195.herokuapp.com`;
+
+const getToken = () => {
+  return localStorage.getItem('token'); //retrieve token from local storage, re-write
+};
+
+// Fetch the latest match result through Get request
+export const getMatchResult = async () => {
+  try {
+    const token = getToken();
+    const response = await fetch(`${BASE_URL}/game/result`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`, 
+      },
+    });
+    if (!response.ok) throw new Error('Network response was not ok');
+    return await response.json();
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+    throw error;
+  }
+};
+
+// Log a new match result - this might depend on how your backend expects to receive a new match result
+export const logMatchResult = async (resultData) => {
+  try {
+    const token = getToken();
+    const response = await fetch(`${BASE_URL}/game/result`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      body: JSON.stringify(resultData),
+    });
+    if (!response.ok) throw new Error('Network response was not ok');
+    return await response.json();
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+    throw error;
+  }
+};
