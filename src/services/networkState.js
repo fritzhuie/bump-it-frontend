917f1e645cbe431dfe932991b8cc497417f27015
fritzhuie <fritzhuie@gmail.com>
2024-02-14 10:10:29 -0500

    add networkState file

---
+++ src/services/networkState.js
@@ -0,0 +1,10 @@
+
+let currentSelectedProfile = null
+let currentSelectedMatchResult = null
+let currentUser = null
+
+export {
+    currentSelectedProfile,
+    currentSelectedMatchResult,
+    currentUser
+}
