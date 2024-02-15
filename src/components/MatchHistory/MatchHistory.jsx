b69b413e57487bce9f019b29f19efa45e8732a7c
fritzhuie <fritzhuie@gmail.com>
2024-02-15 18:12:25 -0500

    add imageS

--- src/components/MatchHistory/MatchHistory.jsx
+++ src/components/MatchHistory/MatchHistory.jsx
@@ -1,34 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-// import CloseIcon from '@mui/icons-material/Close';
-import { getMatchHistory } from '../../services/matchHistoryServices';
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { getMatchHistory } from '../../services/matchHistoryServices'
+import { getProfile } from '../../services/profileServices'
 
 const History = () => {
   const [matchHistory, setMatchHistory] = useState([]);
+  const [profiles, setProfiles] = useState();
   const navigate = useNavigate();
 
   useEffect(() => {
     getMatchHistory()
- 
       .then(history => {
         console.log(history)
         setMatchHistory(history);
+        for(let match of history) {
+
+        }
       })
-        // Fetch profiles for all opponents in the match history
-        // const profilePromises = history.map(match => profileServices.fetchProfile(match.opponentId));
-        // return Promise.all(profilePromises)
-        //   .then(profiles => {
-            // Combine match history with opponent profiles
-            //return history.map() 
-            // => ({ ...match, opponentProfile: profiles[index] }));
-          
-      // })
-      // .then(combinedHistory => {
-      //   setMatchHistory(combinedHistory);
-      // }) 
       .catch(error => {
-        console.error('Failed to fetch match history:', error);
-      });
+        console.error('Failed to fetch match history:', error)
+      })
   }, []);
 
   const handleGoToHistory = () => {
@@ -43,19 +34,25 @@ const History = () => {
   return (
     <div>
       <div className="match-result-header">
-        {/* <button className="close-button" onClick={handleGoToHistory}>
-          <CloseIcon /> {/* Render the "X" icon */}
-        {/* </button> */}
+        <button className="close-button" onClick={handleGoToHistory}>
+          X
+        </button>
         <h1>Match Result</h1>
       </div>
-      <ul>
+      <ul className='match-container'>
         {matchHistory.map((match) => (
-          <li key={match.id} onClick={() => handleMatchClick(match.id)}>
-            {/* <img src={match.userAvatar} alt="User Avatar" />
-            <img src={match.opponentProfile.avatar} alt="Opponent Avatar" /> */}
-            <p>{match.id}</p>
+          <li key={match.id} className='match-row-item' onClick={() => handleMatchClick(match.id)}>
+            <div className='user-avatar'>
+              <img src={`/img/portrait-${match.player_one % 17}.png`} alt="User Avatar" />
+            </div>
+            <div className='opponent-avatar'>
+            <img src={`/img/portrait-${match.player_two % 17}.png`} alt="User Avatar" />
+            </div>
+            
+            <p>{`Match ID: ${match.id}`}</p>
             <p>{match.time_stamp}</p>
-            <p>{match.player_two}</p>
+            <p>{`Player 1: ${match.player_two}`}</p>
+            <p>{`Player 2: ${match.player_two}`}</p>
           </li>
         ))}
       </ul>
